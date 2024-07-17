da891d9f594bef9318fbe4c858e1b682164024b9
Yaveen! <94953863+Yaveen123@users.noreply.github.com>
2024-07-17 15:29:00 +1000

    Delete FizznFroth directory

--- FizznFroth/FizznFroth/public_html/js/history.js
+++
@@ -1,9 +0,0 @@
-function undo(id) {
-    window.location.href = '/mchistory.php?mode=undo&order_id=' + id;
-}
-
-function del(id) {
-    if (confirm(`Are you sure you want to DELETE order ${id}?`)) {
-        window.location.href = '/mchistory.php?mode=delete&order_id=' + id;
-    }
-}
